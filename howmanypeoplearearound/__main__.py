58eb247473f8cee856bcfe1fcefabd74645d30f2
Federico Boschini <federico@nearit.com>
2018-11-19 12:46:47 +0100

    refactor(main): remove phone list

--- howmanypeoplearearound/__main__.py
+++ howmanypeoplearearound/__main__.py
@@ -189,22 +189,6 @@ def scan(adapter, scantime, verbose, number, nearby, jsonprint, out, loop, sort,
                 print("rssi: %s" % str(foundMacs[mac]))
         sys.stdout.write(RESET)
 
-    cellphone = [
-        'Motorola Mobility LLC, a Lenovo Company',
-        'GUANGDONG OPPO MOBILE TELECOMMUNICATIONS CORP.,LTD',
-        'Huawei Symantec Technologies Co.,Ltd.',
-        'Microsoft',
-        'HTC Corporation',
-        'Samsung Electronics Co.,Ltd',
-        'SAMSUNG ELECTRO-MECHANICS(THAILAND)',
-        'BlackBerry RTS',
-        'LG ELECTRONICS INC',
-        'Apple, Inc.',
-        'LG Electronics',
-        'OnePlus Tech (Shenzhen) Ltd',
-        'Xiaomi Communications Co Ltd',
-        'LG Electronics (Mobile Communications)']
-
     cellphone_people = []
     for mac in foundMacs:
         oui_id = 'Not in OUI'
