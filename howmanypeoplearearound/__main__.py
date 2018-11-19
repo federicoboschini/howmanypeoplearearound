9108f9e70cb5b32639954da818f86c31ca6ab435
Federico Boschini <federico@nearit.com>
2018-11-19 12:47:05 +0100

    feat(main): print count in dump

--- howmanypeoplearearound/__main__.py
+++ howmanypeoplearearound/__main__.py
@@ -220,7 +220,7 @@ def scan(adapter, scantime, verbose, number, nearby, jsonprint, out, loop, sort,
 
     if out:
         with open(out, 'a') as f:
-            data_dump = {'cellphones': cellphone_people, 'time': time.time()}
+            data_dump = {'count': num_people, 'devices': cellphone_people, 'time': time.time()}
             f.write(json.dumps(data_dump) + "\n")
         if verbose:
             print("Wrote %d records to %s" % (len(cellphone_people), out))
