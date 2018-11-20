3e63c8d7821f429cc07f87228b5a36eb91000fcf
Federico Boschini <federico@nearit.com>
2018-11-20 15:16:02 +0100

    refactor(analysis): replace `cellphones` with `devices`

--- howmanypeoplearearound/analysis.py
+++ howmanypeoplearearound/analysis.py
@@ -16,7 +16,7 @@ def analyze_file(fname, port):
           pass
     macs_to_add = []
     for data in lines:
-        for c in data['cellphones']:
+        for c in data['devices']:
             if c['rssi'] > -80 and c['mac'] not in macs_to_add:
                 macs_to_add.append(c['mac'])
     mac_data = {mac: {'y': []} for mac in macs_to_add}
@@ -25,14 +25,14 @@ def analyze_file(fname, port):
         rssi = {}
         for mac in macs_to_add:
             rssi[mac] = -100
-            for c in data['cellphones']:
+            for c in data['devices']:
                 if c['mac'] in rssi:
                     rssi[c['mac']] = c['rssi']
         for mac in mac_data:
             mac_data[mac]['y'].append(str(rssi[mac] + 100))
         num['x'].append("'" + datetime.datetime.fromtimestamp(
             data['time']).isoformat().split('.')[0].replace('T', ' ') + "'")
-        num['y'].append(str(len(data['cellphones'])))
+        num['y'].append(str(len(data['devices'])))
 
     mac_names = copy.deepcopy(macs_to_add)
     for i, mac in enumerate(mac_names):
