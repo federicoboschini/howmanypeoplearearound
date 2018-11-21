7e95fdab478749aaefb6757b3d0f2299d1cb3483
Federico Boschini <federico@nearit.com>
2018-11-21 15:23:35 +0100

    refactor(main): better help on -o arg

--- howmanypeoplearearound/__main__.py
+++ howmanypeoplearearound/__main__.py
@@ -63,7 +63,7 @@ def fileToMacSet(path):
 @click.option('-a', '--adapter', default='', help='adapter to use')
 @click.option('-z', '--analyze', default='', help='analyze file')
 @click.option('-s', '--scantime', default='60', help='time in seconds to scan')
-@click.option('-o', '--out', default='', help='output cellphone data to file')
+@click.option('-o', '--out', default='', help='output cellphone data to (unique) file')
 @click.option('-v', '--verbose', help='verbose mode', is_flag=True)
 @click.option('--number', help='just print the number', is_flag=True)
 @click.option('-j', '--jsonprint', help='print JSON of cellphone data', is_flag=True)
